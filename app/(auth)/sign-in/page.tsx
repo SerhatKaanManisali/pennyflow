00cf81b4061c2f3501ec68487632aa0bca37e1bf
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-10-16 02:27:06 +0200

    modified:   app/(auth)/sign-in/page.tsx

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,7 +10,7 @@ const SignIn = () => {
             <Alert variant="default" className="w-fit h-fit mx-6 border-gray-300 text-gray-900 placeholder:text-gray-700 dark:text-gray-300 mb-6 shadow">
                 <AlertTitle>Note</AlertTitle>
                 <AlertDescription className="text-pretty text-center">
-                    Use <span className="font-medium">test@mail.com</span> and <span className="font-semibold">Test1234</span> as your credentials for a quick log in. However you may only use the given bank accounts.
+                    Use <span className="font-medium">test@mail.com</span> and <span className="font-medium">Test1234</span> as your credentials for a quick log in. However you may only use the given bank accounts.
                 </AlertDescription>
             </Alert>
         </section>
